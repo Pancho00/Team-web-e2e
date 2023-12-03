0a8442b117e0b24e15beee298b778281bac357a2
FRANCISCO ANDRES ESPINOSA LILLO <f.espinosa01@ufromail.cl>
2023-12-03 01:15:05 -0300

    test: se comienza a crear test login l-1

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,4 +1,19 @@
 describe("Login", () => {
+	it("[SUCCESS L-1] login", () => {
+		cy.visit("/login", {
+			failOnStatusCode: false,
+		});
+		cy.get('input[id="login-email"').type("f.espinosa01@ufromail.cl");
+		cy.get('input[id="login-password"').type("nkn5OD5D");
+		cy.get("button").click();
+		cy.window().then((win) => {
+			const user = win.localStorage.getItem("user");
+			expect(user).not.to.be.null;
+			expect(user.user).exist;
+			expect(user.token).exist;
+		});
+	});
+
 	it("[Error L-2] invalid credentials", () => {
 		cy.visit("/login", {
 			failOnStatusCode: false,
